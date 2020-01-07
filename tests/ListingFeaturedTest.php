5fcaeb976392bd8694936cf7f7039513e158fb2e
Chizzy <chizoba.princess@gmail.com>
2020-01-07 15:28:44 -0500

    test passed to ensure that getCoc method returns the expected results

--- tests/ListingFeaturedTest.php
+++ tests/ListingFeaturedTest.php
@@ -14,4 +14,15 @@ class ListingFeaturedTest extends TestCase
         $listing = new ListingFeatured($data);
         $this->assertEquals('featured', $listing->getStatus());
     }
+    /** @test */
+    function hasCoc()
+    {
+        $data = [
+            'id' => 1,
+            'title' => 'Test Title',
+            'coc' => 'Test Code of Conduct for Featured Listing'
+        ];
+        $listing = new ListingFeatured($data);
+        $this->assertEquals($data['coc'], $listing->getCoc());
+    }
 }
